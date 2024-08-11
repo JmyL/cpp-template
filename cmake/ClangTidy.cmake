5c970e114ad1566a2cb3d37e013ad6639b6da138
Sungsik Nam <ssnam@vueron.org>
2024-08-11 21:31:15 +0200

    Add clangtidy

---
+++ cmake/ClangTidy.cmake
@@ -0,0 +1,9 @@
+function(AddClangTidy target)
+  find_program(CLANGTIDY_PATH clang-tidy REQUIRED)
+  set_target_properties(
+    ${target}
+    PROPERTIES
+      CXX_CLANGTIDY
+      "${CLANGTIDY_PATH};-checks=-*,performance-*,modernize-*,portability-*"
+  )
+endfunction()
