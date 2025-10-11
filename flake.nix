befeaf8fe028ee85e391b9325c90ccbe68d1b7fa
Sungsik <sungsik@ultihash.io>
2025-10-11 14:01:38 +0200

    Set CC and CXX on flake.nix rather than on .envrc

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
         buildInputs =
           [ clang ninja cmake ccache boost.dev cppcheck doxygen lcov ];
 
+        shellHook = ''
+                    export CC=clang
+                    export CXX=clang++
+          	  '';
         CMAKE_PREFIX_PATH = "${boost.dev}";
         CPLUS_INCLUDE_PATH = "${boost.dev}/include";
       };
