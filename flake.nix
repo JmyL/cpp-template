03a1fd00e6b1794e1fcc6b5bfb600a7aa5586bc9
Sungsik <sungsik@ultihash.io>
2025-10-11 13:38:57 +0200

    Format flake.nix using nixfmt

--- flake.nix
+++ flake.nix
@@ -1,26 +1,18 @@
 {
   description = "clang + Boost development environment (CMake ready)";
 
-  # See https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/compilers/llvm/default.nix to find correct commit id for specific clang version
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  # See https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/compilers/llvm/default.nix 
+  # to find correct commit id for specific clang version
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
-      buildInputs = [
-        clang
-        ninja
-        cmake
-        ccache
-        boost.dev
-        cppcheck
-        doxygen
-        lcov
-      ];
+    devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
+      mkShell {
+        buildInputs =
+          [ clang ninja cmake ccache boost.dev cppcheck doxygen lcov ];
 
-      CMAKE_PREFIX_PATH = "${boost.dev}";
-      CPLUS_INCLUDE_PATH = "${boost.dev}/include";
-    };
+        CMAKE_PREFIX_PATH = "${boost.dev}";
+        CPLUS_INCLUDE_PATH = "${boost.dev}/include";
+      };
   };
 }
