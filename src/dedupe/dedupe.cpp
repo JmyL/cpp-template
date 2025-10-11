229f1008b0cfd0d77f4bcbe74af6bd5ae702be31
Sungsik <sungsik@ultihash.io>
2025-10-11 13:37:36 +0200

    Implement dedupe

--- src/dedupe/dedupe.cpp
+++ src/dedupe/dedupe.cpp
@@ -1,15 +1,21 @@
+#include <iostream>
 #include <ranges>
 #include <set>
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
-int main(void) { //
+int main(void) {
     auto input = std::vector<std::string>(
         {"abc", "dcf", "efg", "abc", "abc", "dcf", "hij"});
-    std::set<std::string> output;
-    for (const auto &s : std::views::reverse(input)) {
-        output.insert(s);
-    }
-    std::cout << *output.begin() << std::endl;
+    // std::set<std::string> output;
+    // for (const auto &s : std::views::reverse(input)) {
+    //     output.insert(s);
+    // }
+
+    std::ranges::sort(input);
+
+    std::cout << *input.begin() << std::endl;
+    std::cout << *input.rbegin() << std::endl;
+    
 }
