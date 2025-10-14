2e41c222ca6174fdd6ed3fb6edc7c7bb0787c47b
Sungsik <sungsik@ultihash.io>
2025-10-14 21:40:57 +0200

    Add test cases for lower_bound and upper_bound

--- test/scribble/scribble_test.cpp
+++ test/scribble/scribble_test.cpp
@@ -13,3 +13,13 @@ auto get_vector() {
 TEST(AScribble, SumAddsTwoInts) {
     EXPECT_THAT(get_vector(), ElementsAre(1, 2, 3, 4)); //
 }
+
+TEST(AMap, LowerBoundAndUpperBound) {
+    std::map<int, std::string> m = {{1, "a"}, {3, "b"}, {5, "c"}};
+
+    auto it = m.lower_bound(3);
+    EXPECT_EQ(3, it->first);
+
+    it = m.upper_bound(3);
+    EXPECT_EQ(5, it->first);
+}
