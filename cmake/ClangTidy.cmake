e819c5982c2ae8704670339a93676dea7bf97755
Sungsik <sungsik@ultihash.io>
2025-10-21 15:38:42 +0200

    Take cmake modules out: to another repo, cmake-modules

--- cmake/ClangTidy.cmake
+++
@@ -1,7 +0,0 @@
-function(add_clangtidy target)
-    find_program(CLANGTIDY_PATH clang-tidy REQUIRED)
-    set_target_properties(
-        ${target}
-        PROPERTIES CXX_CLANGTIDY "${CLANGTIDY_PATH}"
-    )
-endfunction()
--- cmake/CppCheck.cmake
+++
@@ -1,9 +0,0 @@
-function(add_cppcheck target)
-    find_program(CPPCHECK_PATH cppcheck REQUIRED)
-    set_target_properties(
-        ${target}
-        PROPERTIES
-            CXX_CPPCHECK
-                "${CPPCHECK_PATH};--enable=style,performance,portability;--error-exitcode=10;--std=c++${CMAKE_CXX_STANDARD};--suppress=missingIncludeSystem"
-    )
-endfunction()
